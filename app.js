0bc7d564ab51a2e76ef45007d0d2527442537bb6
shohruxon <107299025+shohruxon@users.noreply.github.com>
2022-06-11 14:09:12 +0500

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 const animals = ['wolf', 'tigar', 'lion']
 app.get('/', (req, res) => {
     res.write('bosh sahifa');
@@ -10,9 +12,24 @@ app.get('/', (req, res) => {
 app.get('/api/animals', (req, res) => {
     res.send(animals);
 
+})
+
+
+app.post('/add/animals', (req, res) => {
+    animals.push(req.body)
+    console.log(req.body);
+    res.send(req.body);
+})
+app.put('/api/animals', (req, res) => {
+    animals.push('dog')
+    res.send('asdasd')
+})
+app.delete('/api/animals', (req, res) => {
+    animals.pop('dog')
+    res.send('ochirildi')
 
 })
-app.post('')
+
 
 app.listen(port, () => {
     console.log(`Port listening ${port}`);
